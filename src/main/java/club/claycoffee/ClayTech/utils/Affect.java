2915168dd521fdc8436ac886e9a9341d44c14484
ClayCoffee <1020757140@qq.com>
2020-03-03 19:57:19 +0800

    ClayTech

---
+++ src/main/java/club/claycoffee/ClayTech/utils/Affect.java
@@ -0,0 +1,34 @@
+package club.claycoffee.ClayTech.utils;
+
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import club.claycoffee.ClayTech.utils.Utils;
+import me.mrCookieSlime.Slimefun.api.Slimefun;
+
+public class Affect {
+	@SuppressWarnings("deprecation")
+	public static void AffectCheck(Player d, Player e) {
+		if(Slimefun.hasUnlocked(d.getPlayer(),d.getItemInHand(), true)) {
+			try {
+				if (Utils.ExitsInList("¡ì7·´µ¯»ºÂý III", Utils.getLore(e.getInventory().getBoots()))) {
+					d.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 40, 2));
+				}
+				if (Utils.ExitsInList("¡ì7Ê§Ã÷ V", Utils.getLore(d.getItemInHand()))) {
+					e.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 100, 4));
+				}
+				if (Utils.ExitsInList("¡ì7»ºÂý V", Utils.getLore(d.getItemInHand()))) {
+					e.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 100, 4));
+				}
+				if (Utils.ExitsInList("¡ì7·´Î¸ V", Utils.getLore(d.getItemInHand()))) {
+					e.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 100, 4));
+				}
+				if (Utils.ExitsInList("¡ì7ÖÐ¶¾ III", Utils.getLore(d.getItemInHand()))) {
+					e.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 40, 2));
+				}
+			} catch (NullPointerException err2) {
+			}
+		}
+	}
+}
